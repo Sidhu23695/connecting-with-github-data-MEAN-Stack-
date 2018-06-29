c6f285cf39b3c434658e7f096f3438a42c3a62b5
ugendar <ugendar@askpundit.com>
2018-06-29 09:00:49 +0530

    update

--- routes/user.js
+++ routes/user.js
@@ -58,46 +58,6 @@ userRouter.get('/getInfo/:username', async (req, res) => {
   }
 });
 
-// API to fetch and save searched users
-
-userRouter.get('/getInfo/:username', async (req, res) => {
-  try {
-    let validInputs = ['username'];
-    let isValid = globalFunctions.requiredFields(req.params, validInputs);
-    if (!isValid) {
-      return res.status(400).json({
-        message: 'Required fields missing'
-      });
-    }
-    const githubData = await fetch(`https://api.github.com/users/${req.params.username}/repos`);
-    const json = await githubData.json();
-    let formData = {
-      gitHubUsername: 'ugendarkumar',
-      publicRepositories: []
-    };
-    for (let i = 0; i < json.length; i++) {
-      let repData = {
-        repoName: json[i].name,
-        repoUpdatedDate: json[i].updated_at,
-        repoCreatedDate: json[i].created_at,
-        nodeId: json[i].node_id,
-        id: json[i].id
-      };
-      formData.publicRepositories.push(repData);
-    }
-    let saveGitUser = await new User(formData).save()
-    return res.status(200).json({
-      data: saveGitUser
-    });
-
-  } catch (e) {
-    console.log(e);
-    return res.status(500).json({
-      message: 'Internal server error'
-    });
-  }
-});
-
 
 
 
